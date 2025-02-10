8534f1869628f0308890a8dc3830c8c13dc028bd
mauricio <mauriciomoldesquaresma@gmail.com>
2025-02-10 13:53:34 +0100

    Google Analytics tracking

--- dengen-website/src/index.js
+++ dengen-website/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -11,7 +12,30 @@ root.render(
   </React.StrictMode>
 );
 
+const trackingId = "G-0EYMEP67W8";  // Replace with your actual Google Analytics Tracking ID
+
+ReactDOM.render(
+  <>
+    <Helmet>
+      {/* Google Analytics Script */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${trackingId}');
+        `}
+      </script>
+    </Helmet>
+    <App />
+  </>,
+  document.getElementById("root")
+);
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
