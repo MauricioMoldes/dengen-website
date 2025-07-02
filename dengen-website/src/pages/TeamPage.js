9529381d738994f64865051411356c41425a6e24
MauricioMoldes <mauriciomoldesquaresma@gmail.com>
2025-07-02 13:55:25 +0200

    team page

--- dengen-website/src/pages/TeamPage.js
+++ dengen-website/src/pages/TeamPage.js
@@ -8,73 +8,73 @@ function TeamPage() {
       name: "Frederik Otzen Bagger",
       role: "Project Lead",
       bio: "Frederik Bagger is an experienced bioinformatician specializing in genomic data analysis. She leads the DenGen project, overseeing the research and ensuring the accuracy of the dataset.",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3EpwdMA04GdtraUTmWvqFjXlpPOfQ_APVTA&s",
+      image: `Frederik_Bagger.jpg`
     },
     {
       name: "Christian Baudet",
       role: "Senior Bioinformatician",
       bio: "Christian Baudet brings expertise in computational biology and genetic variant interpretation. He works on the pipelines and quality control processes for the DenGen database.",
-      image: "https://media.licdn.com/dms/image/v2/C4D03AQEG2jkTm5o8yA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1516532687196?e=2147483647&v=beta&t=BDBG0ZC9beyKMprh2b2fEztKzHMj6iEku1kRV621Juw",
+      image: `Christian_Baudet.jpg`
     },
     {
       name: "Luca Robinson",
       role: "Genomics Researcher",
       bio: "Luca Robinson is responsible for analyzing large-scale genomic datasets and developing machine learning models to interpret the DenGen data.",
-      image: "https://media.licdn.com/dms/image/v2/D4D03AQHf0Q-Px14RQw/profile-displayphoto-shrink_200_200/B4DZYPmOG7GwAc-/0/1744018400172?e=1752710400&v=beta&t=kq4Ae_7WUbjyeFEP3HHMGppCFI9ezkZaMWDS5dIOitI",
+      image: `Luca_Robsinson.jpg`
     },
     {
       name: "Andreas Jespersen",
       role: "Genomics Researcher",
       bio: "Andreas Jespersen focuses on the development of the DenGen website and its various features, ensuring seamless user experience and performance.",
-      image: "https://media.licdn.com/dms/image/v2/C4D03AQHW6TyDz841QQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1551367633591?e=1752710400&v=beta&t=4yBQMTckKZP430_-_lZRKKrGk42LvHTEPj2tOQC_Zvo",
+      image: `andreas_jesperson.jpg`
     },    
     {
       name: "Mauricio Moldes",
       role: "Senior Software Engineer",
       bio: "Mauricio Moldes contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/C5603AQGBpiN4B1SZyA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1517069062586?e=1752710400&v=beta&t=Ju58TDByr9JXBwilL3mP0nEe4ivFj40FYYPgx8BzCXg",
+      image: `mauricio_moldes.jpg`
     },
     {
       name: "Gül Sude Demircan",
       role: "Genomics Researcher",
       bio: "Gül Demircan contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/C4D03AQFTdQsf2fLU6Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1577970668713?e=1752710400&v=beta&t=vLKHcLGAIjV0mhBo4C08IJKbnIubE1T5MMwfmmhXKqQ",
+      image: `gul_sude.jpg`
     },
     {
       name: "Xin Sun",
       role: "Senior Software Engineer",
       bio: "Xin Sun contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/D4E03AQHTZ3BKonV5WQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1678565793476?e=1752710400&v=beta&t=ftM75iVRqLlaGJeIdRtk58-XqaR-Q5H0_JJASIwyKow",
+      image: `xin_sun.jpg`
     },
     {
       name: "Miyako Kodama Henriksen",
       role: "Genomics Researcher",
       bio: "Miyako Henriksen contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/C5603AQF0_9h7Z2W1Dg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1516487959835?e=1752710400&v=beta&t=UvYBe9AslCjlDXkLwtZadbYT4iNzf6Jx6FoDWnY2jVo",
+      image: `Miyako_Henriksen.jpg`
     },    
     {
       name: "Filipe Garrett Vieira",
       role: "Genomics Researcher",
       bio: "Filipe Garrett Vieira contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://researchprofiles.ku.dk/assets/no-portrait-473c6d005990baa1f418d9c668dcd4ec.png",
+      image: `Filipe_Vieira.jpg`
     },
     {
       name: "Tobias Overlund Stannius",
       role: "Genomics Researcher",
       bio: "Tobias  Stannius contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/D4E03AQHCPPgkx1xobA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1673174198227?e=1752710400&v=beta&t=AZHg4oEDFwf9tJfjGiadkNys8nO4HVi2OVXvhqcrKAs",
+      image: `Tobas_Stannius.jpg`
     },
     {
       name: "Javiera Laing Carmona",
       role: "Genomics Researcher",
       bio: "Javiera Carmona contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/C4E03AQF65uHv1pSz6A/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1650738837009?e=1752710400&v=beta&t=vviNojLV8QA6-c_jASTJE4OJcIALp8TXtNqYpJTi2Bg",
+      image: `Javiera_Carmona.jpg`
     },
     {
       name: "Rasmus Lykke Marvig",
       role: "Genomics Researcher",
       bio: "Rasmus Lykke Marvig contributes to the scientific research aspect of DenGen, focusing on genomics, variant calling, and collaboration with clinical researchers.",
-      image: "https://media.licdn.com/dms/image/v2/D4D03AQH_ISqSkELZUg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1725607462866?e=1756944000&v=beta&t=mtX78eb87uXxKbvk446-AX_hyxsKLmyK0Mx-UqKgC8w",
+      image: `Rasmus_Marvig.jpg`
     }
 
     
